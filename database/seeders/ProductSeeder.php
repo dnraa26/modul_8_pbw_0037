8e1bc0b8780ec65d1084894a64e6f9c823b27792
dnraa26 <145641411+dnraa26@users.noreply.github.com>
2024-10-22 13:32:25 +0700

    Modul 8

---
+++ database/seeders/ProductSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Product;
+use Illuminate\Database\Seeder;
+
+class ProductSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Product::create([
+            'name' => 'Product 1',
+            'description' => 'Description for Product 1',
+            'price' => 100.00
+            ]);
+    }
+}
